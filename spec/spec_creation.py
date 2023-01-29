63c4031d3c4a9d092c344091578f66b42f0e7841
Matt <mrehberg@mines.edu>
2023-01-28 17:08:07 -0800

    latex build

--- spec/spec_creation.py
+++ spec/spec_creation.py
@@ -6,21 +6,33 @@ Created on Wed Jan 25 20:39:03 2023
 """
 
 import pandas as pd
+import os
 
+global defined_terms
 
-def escapeLatex(text):
+def escapeLatex(text, link):
     text = text.replace('$', '\$')
     text = text.replace('_', '\_')
     text = text.replace('%', '\%')
-    
+    text = text.replace('#', ' ')
+    if link:
+        text = createLink(text)
     return text
 
-
-
-
+def createLink(text):
+    text = text.replace('{ref:', '\\nameref{ref:')
+    for key in defined_terms:
+        value = defined_terms[key]
+        term = '{term:'+key+'}'
+        label = 'term:' + key
+        link = '\hyperref['+label+']{'+value+'}'
+        text = text.replace(term, link)
+    return text
 
 df = pd.read_excel('Requirements.xlsx', sheet_name='Spec', header=2, usecols=lambda x: 'Unnamed' not in x,).fillna('')
 df = df[df.columns[0:4]]
+defined_terms=dict()
+
 
 with open('spec-info.tex', 'w') as f:
     # f.write('readme')
@@ -48,9 +60,25 @@ with open('spec-info.tex', 'w') as f:
         prev_section = section
         
         if prev_sub != sub:
-            f.write(r'\subsubsection{{{}}}'.format(sub))
+            f.write(r'\subsubsection{{{}}}'.format(escapeLatex(sub, False)))
             f.write('\n')
         prev_sub = sub
         
-        f.write('{}'.format(escapeLatex(text)))
+        
+       
+        
+        # If the value is a defined term, add it to the defined term dictionary
+        if section == 'Defined Term':
+            defined_terms.update({sub:text})
+            f.write('\label{{term:{}}}'.format(escapeLatex(sub, False)))
+        else:
+            f.write('\label{{ref:{}}}'.format(escapeLatex(sub, False)))
+        
+        
         f.write('\n')
+        f.write('{}'.format(escapeLatex(text, True)))
+        f.write('\n')
+    
+  
+for i in range(3):
+    os.system("pdflatex bti-spec.tex")
