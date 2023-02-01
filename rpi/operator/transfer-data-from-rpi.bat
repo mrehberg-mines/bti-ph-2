d9bc723a4fa6ad7a39048d52887d837a78567de1
Matt <mrehberg@mines.edu>
2023-01-31 22:05:55 -0800

    updated gitignore and transfered file

--- rpi/operator/transfer-data-from-rpi.bat
+++ rpi/operator/transfer-data-from-rpi.bat
@@ -1,13 +1,11 @@
 @echo off
-set path=%cd%
 
-"C:\Program Files (x86)\WinSCP\WinSCP.com" ^
-  /ini=nul ^
+"C:\Users\HelloWorld\AppData\Local\Programs\WinSCP\WinSCP.com" ^
   /command ^
-    "open sftp://pi:rpi@192.168.0.2/ -hostkey=""ssh-ed25519 255 9NQii05JQ8SdX1qCOsjF8yWjEyO0Q+0QtCi4OtVWzm0=""" ^
-    "cd pi/home/_git_code/bti-ph-2/rpi/rpi-runtime" ^
-    "lcd %path%/rpi-data^
-    "get *" ^
+    "open sftp://pi:rpi@192.168.0.241/ -hostkey=""ssh-ed25519 255 F90yjkDmQFFmfLMxje5x/IbnnmdNwCQBLXEC0dZQLlM""" ^
+    "cd /home/pi/git_code/bti-ph-2/rpi/rpi-runtime" ^
+    "lcd C:\Users\HelloWorld\Documents\_git_code\bti-ph-2\rpi\operator\rpi-data" ^
+    "get -neweronly *" ^
     "exit"
 
 set WINSCP_RESULT=%ERRORLEVEL%
