4137548e4017d6225107b62aa3134e01ca96670f
Matt <mrehberg@mines.edu>
2023-02-06 19:01:49 -0800

    wifi connected

--- rpi/operator/client.py
+++ rpi/operator/client.py
@@ -1,6 +1,6 @@
 import socket
 
-TCP_IP = '192.168.137.118' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
+TCP_IP = '192.168.0.241' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Move To Left"
--- rpi/operator/server.py
+++ rpi/operator/server.py
@@ -1,17 +1,17 @@
 import socket
 
-TCP_IP = '192.168.137.118' # this IP of my pc. When I want raspberry pi 2`s as a server, I replace it with its IP '169.254.54.195'
+TCP_IP = '192.168.0.241' # this IP of my pc. When I want raspberry pi 2`s as a server, I replace it with its IP '169.254.54.195'
 TCP_PORT = 5005
-BUFFER_SIZE = 20 # Normally 1024, but I want fast response
+BUFFER_SIZE = 1024 # Normally 1024, but I want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
-s.listen(10)
-
-conn, addr = s.accept()
-print ('Connection address:', addr)
+s.listen(5)
 while 1:
+    conn, addr = s.accept()
+    print ('Connection address:', addr)
     data = conn.recv(BUFFER_SIZE)
+    if not data: break
     print ("received data:", data)
     conn.send(data)  # echo
     conn.close()
